99cb454a10a697ad125bf97b01d8b5cb139cd7e9
matnex-mix <jolaoshobatmat@gmail.com>
2022-04-13 13:17:33 +0100

    switched production to use local uploads

--- config/environments/production.php
+++ config/environments/production.php
@@ -6,3 +6,4 @@ define('WP_DEBUG', true);
 define('SCRIPT_DEBUG', true);
 /** Disable all file modifications including updates and update notifications */
 define('DISALLOW_FILE_MODS', true);
+define('S3_UPLOADS_USE_LOCAL', true);
