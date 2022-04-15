4ebf6c0967c1ca2d9dae485cbf17eee7a9591b26
matnex-mix <jolaoshobatmat@gmail.com>
2022-04-15 12:26:29 +0100

    spelling error

--- web/app/themes/busicol/components/home-contact.php
+++ web/app/themes/busicol/components/home-contact.php
@@ -1,7 +1,7 @@
 <div class="bg-success d-flex justify-content-center align-items-center text-white" style="height: 60vh;">
 	<div class="text-center" style="max-width: 540px">
 		<h3>Do you Have any Project?</h3>
-		<p class="my-4">Don’t hesitate to contact us for any of your software projects. After success discussion, you get a quote within 3 business days.</p>
+		<p class="my-4">Don’t hesitate to contact us for any of your software projects. After successful discussion, you get a quote within 3 business days.</p>
         <a href="<?php show( get_the_permalink( get_page_by_title('contact')->ID ) ); ?>" class="btn btn-light bg-transparent btn-lg ml-4 text-white font-weight-light" style="height: 3.4rem; width: 200px">
             <i class="fa fa-caret-right"></i>&nbsp; Contact Us
         </a>
