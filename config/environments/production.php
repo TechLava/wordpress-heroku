146188ba625c80ebdae5336298a8bcf6a7cda1f9
matnex-mix <jolaoshobatmat@gmail.com>
2022-04-15 07:03:15 +0100

    allow s3 uploads for production

--- config/environments/production.php
+++ config/environments/production.php
@@ -7,4 +7,3 @@ define('WP_DEBUG', true);
 define('SCRIPT_DEBUG', true);
 /** Disable all file modifications including updates and update notifications */
 define('DISALLOW_FILE_MODS', true);
-define('S3_UPLOADS_USE_LOCAL', true);
